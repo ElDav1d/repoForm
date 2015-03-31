fbe396aa4c5a8459d156e2f619f18f9f969f726f
ElDav1d <dvivoes@yahoo.es>
2015-03-31 10:24:15 +0200

    Change 'NOMBRE' input into 'user-comments'

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([39.464741, -0.375517], 20);
+var map = L.map('map').setView([39.464741, -0.375517],15);
 
 var Esri_WorldGrayCanvas = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
